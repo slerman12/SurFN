c31cb69b197a1cc4b89f271ddd80c0a0db303495
samlerman <slerman@ur.rochester.edu>
2021-01-12 21:01:39 -0500

    sweep

--- sweep_nodes.py
+++ sweep_nodes.py
@@ -9,7 +9,7 @@ algs = ["PPO", "A2C"]
 aggs = ["sign", None]
 seeds = [random.randint(0, 200), random.randint(200, 400), random.randint(400, 600), random.randint(600, 800), random.randint(800, 1000)]
 
-args = list(itertools.product(*[envs, algs, aggs, seeds]))
+args = itertools.product(*[envs, algs, aggs, seeds])
 
 with Pool(32) as processing_pool:
     processing_pool.map(run, args)
