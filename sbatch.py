3c39035fd963186f8463bac6a4c246bc560ee2e0
samlerman <slerman@ur.rochester.edu>
2021-01-14 14:23:59 -0500

    slurm

--- sbatch.py
+++ sbatch.py
@@ -26,7 +26,6 @@ def slurm_script_generalized():
 {}
 module load {}
 module load python3/3.8.6
-module load yaml
 python3 {} {}
 """.format("-c 1" if args.cpu else "-p gpu", "" if args.cpu else "--gres=gpu", args.name, args.name,
            "#SBATCH -C K80" if args.bigger_gpu else "#SBATCH -C V100" if args.biggest_gpu else "",
